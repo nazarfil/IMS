319181c446ec9305d9d0c616459f8e9d99a256b3
nazarfil <n.filipchuk@gmail.com>
2017-08-16 21:13:32 +0200

    clean up name

--- testnet/newAcc.sh
+++
@@ -1,2 +0,0 @@
-#/bin/bash -e
-geth --datadir "/home/fnazar/eth_test_net" account new
--- testnet/secondNode.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -e
-
-geth --datadir "/home/fnazar/eth_test_net" --networkid 2104  --identity "Second Node" --cache 128 --rpc --rpcapi "db, eth, net, web3" --port 30304 --ipcapi "admin,db,eth,debug,miner,net,shh,txpool,personal,web3" --rpcport 8547 --rpccorsdomain "*" --nat "any" --mine
